acadf4b6379f0d735531b0b6e4599995bc85787e
TomiSalo <tomi.a.salo@outlook.com>
2020-01-25 21:32:26 +0200

    Router alku

---
+++ src/Maat/Maat.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react'
+import classes from './Maat.module.css'
+import axios from 'axios'
+import {Route, Link} from 'react-router-dom';
+import Saat from '../Saat/Saat';
+
+
+class Maat extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          countries: [],
+          searchTerm: '',
+          
+        }
+      }
+  //Tämän lifecycle hookin avulla haetaan data tietokannasta
+  componentDidMount() {
+    axios
+      .get('https://restcountries.eu/rest/v2/all')
+      .then(response => {
+        this.setState({countries: response.data})
+      });
+  }
+  //Tämän metodin avulla osataan yhdistää maan nimi ja input arvoon.
+  handleChange = ({target: {name, value}}) => {
+    this.setState({[name]: value})
+  }
+  //löytää hakemat myös pienillä kirjaimilla
+	handleResultClick = ( countryName ) => {
+		this.setState({ searchTerm: countryName.toLowerCase() });
+	}
+
+  render() {
+    //Tässä ensiksi haetaan maiden data ja tehdään lista vain maiden nimistä ja muutetaan niiden kirjaimet pieniksi, samoin hakusanat pieniksi
+    const countriesToShow = this.state.countries.filter(country => country.name.toLowerCase().includes(this.state.searchTerm.toLowerCase()))
+    const text = 'Too many matches, specify another filter.';
+  return (
+    <div className={classes.Container}>
+        
+    
+            
+            <input placeholder="Find a country..." className={classes.Inputti} name="searchTerm" value={this.state.searchTerm} onChange={this.handleChange} />  
+        <div>
+            { //Jos maita löytyy enemmän kuin 10 niin tulostaa tarkenna tietoa
+               countriesToShow.length > 10 
+              ? text
+              //Jos löydettujen maiden määrä on suurempi kuin 1 niin se ehdottaa vaihtoehtoina löydetyt
+              : countriesToShow.length > 1
+              //Ja muutetaan vaihtoehdot napeiksi, joita voi valita
+              ? countriesToShow.map(country => (
+                  <button
+                    key={country.name}
+                    name="searchTerm"
+                    value={country.name}
+                    onClick={this.handleChange}
+                    style={{margin: '10px', padding: '10px', fontSize: '1rem', borderRadius: '10px', border: 'solid, 1px', display: 'block'}}
+                    >
+                      {country.name}
+                    
+                  </button>
+              ))
+              //Jos haku löytää tasan yhden oikean vaihtoehdon niin tulostetaan tiedot
+              : countriesToShow.length === 1
+              ? countriesToShow.map(country => (
+                <div key={country.name}>
+                  <h2>{country.name}</h2>
+                  <p style={{margin: '10px'}}>Capital: {country.capital}</p>
+                  <p>Population: {country.population}</p>
+                  <img style={{margin: '10px'}} alt={`${country.name} flag`} src={country.flag} width="200" />
+                </div>
+              ))
+              : 'No matches were found.'  
+          }   
+           <Route path="/saat" exact component={Saat} />
+        </div>    
+    </div>
+  );
+  }
+}
+
+
+export default Maat;
--- src/Saat/Saat.js
+++ src/Saat/Saat.js
@@ -6,33 +6,61 @@ class Saat extends Component {
         super(props)
         this.state = {
             citys: [],
-            filter: ''
+            searchTerm: ''
         }
 }
 componentDidMount() {
     axios
-        .get('https://api.weatherstack.com/0bc72171eccbd4e4ea355e07d77f6456')
+        .get('https://api.weatherstack.com/0bc72171eccbd4e4ea355e07d77f6456.json')
         .then(response => {
-            this.setState({citys: response.name})
-        })
+            this.setState({citys: response.data})
+        });
+}
+handleChange = ({target: {name, value}}) => {
+    this.setState({[name]: value})
+}
+
+//löytää hakemat myös pienillä kirjaimilla
+handleResultClick = ( countryName ) => {
+    this.setState({ searchTerm: countryName.toLowerCase() });
 }
 
 render() {
-
+//Etsii osuvat kaupungit
+const showCity = this.state.citys.filter(country => country.name.toLowerCase().includes(this.state.searchTerm.toLowerCase()))
 
 return(
-<div>
+    <div>
+        <input onChange={this.handleChange} type='text' name="searchTerm" value={this.state.searchTerm} />
+        <div>
+        {
+            showCity.length > 10 
+            ? 'Liian monta tulosta löydetty, tarkenna hakua' 
+            : showCity > 1
+            ? showCity.map(country => (
+                <button
+                value={country.name}
+                name="filter"
+                onClick={this.handleChange}
+                style={{margin: '10px', padding: '10px', fontSize: '1rem', borderRadius: '10px', border: 'solid, 1px', display: 'block'}}
+                >
+                    {country.name}
+                </button>
+            ))
+            : showCity === 1
+            ? showCity.map(country => (
+                <div key={country.name}>
+                    <h1>{country.name}</h1>
+                </div>
+            ))
+            : 'No matches were found.'  
 
-</div>
+        }
+        </div>
+    </div>
 
+);
 
+}}
 
-)
-
-
-
-
-}
-}
-
-export default saat;
+export default Saat;
